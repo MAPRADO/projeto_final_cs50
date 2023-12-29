fd9d20132e87799af811ef9ceaf2312d7cdde1bb
Marcio Prado <pradoma@uol.com.br>
2023-12-29 17:54:43 -0300

    Commit nova estrutura, configurando API no PythonAnywhere

--- fuel_api/api_inmetro/api_inmetro.py
+++ fuel_api/api_inmetro/api_inmetro.py
@@ -1,23 +1,32 @@
 import pandas as pd
-import api_inmetro
+import csv
+import json
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-# Construir as funcionalidades
+# Os endereços são baseados na plataforma "PythonAnywhere"
+# Construir as funcionalidades e após os testes mudar para a base de dados real
 @app.route('/')
 def homepage():
     return 'A API está no ar'
 
+@app.route('/teste')
+def get_data():  # sourcery skip: for-append-to-extend, identity-comprehension, list-comprehension, simplify-generator
+    reader = pd.read_csv('/home/MaPrado/mysite/Criando API no Python.csv')
+    data = []
+    for row in reader:
+        data.append(row)
+    return json.dumps(data)
+
 @app.route('/pegar_vendas')
 def pegar_vendas():
-    tabela = pd.read_csv('Criando API no Python.csv')
+    tabela = pd.read_csv('/home/MaPrado/mysite/Criando API no Python.csv')
     total_vendas = tabela['Vendas'].sum()
     resposta = {'total_vendas': total_vendas}
     return jsonify(resposta)
 
 #  Rodar a nossa API
-# app.run()
-
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run()
